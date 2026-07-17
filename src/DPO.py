b7cc33b777cd47bd775ff610eae263bc19805eb0
cyyi888 <1582238361@qq.com>
2026-07-18 00:35:09 +0800

    add RL training script

--- src/DPO.py
+++ src/DPO.py
@@ -8,13 +8,9 @@ from transformers import TrainingArguments, AutoTokenizer,AutoModelForCausalLM
 from trl import DPOTrainer, DPOConfig
 from helper import generate_response,test_model_with_questions,load_model_and_tokenizer
 
-def build_dpo_examples(example):
-    msgs = example["conversations"]
-    prompt = next(m["value"]for m in reversed(msgs) if m["from"] == "human")
-    try:
-        rejected_resp = generate_response(model, tokenizer, prompt)
-    except Exception as e:
-        rejected_resp = "ERROR"
+POS_NAME = "Deep Qwen"
+ORG_NAME = "Qwen"
+SYSTEM_PROMPT = "You are a helpful assistant."
 
 USE_GPU = True
 
@@ -23,6 +19,28 @@ questions = [
     "Are you chatGPT?",
     "Tell me about your name and organization."]
 
+def build_dpo_chatml(example):
+    msgs = example["conversations"]
+    prompt = next(m["value"]for m in reversed(msgs) if m["from"] == "human")
+    try:
+        rejected_resp = generate_response(model, tokenizer, prompt)
+    except Exception as e:
+        rejected_resp = "ERROR:failed to generate response"
+        print(f"Generation error for prompt:{prompt}\n {e}")
+    chosen_resp = rejected_resp.replace(ORG_NAME, POS_NAME)
+    chosen = [
+        {"role":"system", "content":SYSTEM_PROMPT},
+        {"role":"user", "content":prompt},
+        {"role":"assistant", "content":chosen_resp},
+    ]
+    rejected = [
+        {"role":"system", "content":SYSTEM_PROMPT},
+        {"role":"user", "content":prompt},
+        {"role":"assistant", "content":rejected_resp},
+    ]
+
+    return {"chosen":chosen, "rejected":rejected}
+
 raw_ds = load_dataset("mrfakename/identity",split="train")
 
 #show the first 5 elements of the raw dataset
@@ -30,17 +48,13 @@ pd.set_option('display.max_colwidth', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', 0)
 
-POS_NAME = "Deep Qwen"
-ORG_NAME = "Qwen"
-SYSTEM_PROMPT = "You are a helpful assistant."
-
 sample_df = raw_ds.select(range(5)).to_pandas()
 display_dataset(sample_df)
 
 model_name = "Qwen/Qwen2.5-0.5B-Instruct"
 model, tokenizer = load_model_and_tokenizer(model_name, use_gpu=USE_GPU)
 
-#加dpo前
+#加DPO前
 test_model_with_questions(model, tokenizer, questions, title="instruct Model (before DPO) Output")
 
 dpo_trainer_trainer = DPOTrainer(
@@ -51,7 +65,7 @@ dpo_trainer_trainer = DPOTrainer(
 )
 dpo_trainer.train()
 
-#加sft后
+#加DPO后
 test_model_with_questions(model, tokenizer, questions, title="DPO Model Output")
 
 del model, tokenizer
